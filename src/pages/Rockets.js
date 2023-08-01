292bbca9713a1bf27fb11a9482ef96ac6fd1c60f
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-08-01 19:55:38 +0300

    Add features for displaying rockets

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,29 @@
-import React from 'react';
+// import React from 'react';
+// import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
-function Rockets() {
-  return <div>Rockets</div>;
+function Rocket() {
+  const rocket = useSelector((store) => store.rockets);
+  console.log(rocket);
+  // return (
+  //   <div className="single-rocket">
+  //     <img src={rocket.image} alt="rocket" />
+  //     <div className="rocket-info">
+  //       <h1>{rocket.name}</h1>
+  //       <p>{rocket.description}</p>
+  //       <Button type="submit">Reserve Rocket</Button>
+  //     </div>
+  //   </div>
+  // );
 }
 
-export default Rockets;
+export default Rocket;
+
+Rocket.propTypes = {
+  rocket: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
