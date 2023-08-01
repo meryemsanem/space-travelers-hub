20a132eba0dfc4c7aad50473f11561511b66209f
Mahdi Noori <mahdinoori2389@gmail.com>
2023-08-01 15:08:57 +0430

    Add missions slice to the missionsSlice.js

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const apiUrl = 'https://api.spacexdata.com/v3/missions';
@@ -19,3 +19,26 @@ export const fetchMissions = createAsyncThunk('missions/fetchMissions', async ()
     throw Error(error);
   }
 });
+
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  extraReducers: (bulider) => {
+    bulider.addCase(fetchMissions.pending, (state) => ({
+      ...state,
+      loading: true,
+    }))
+      .addCase(fetchMissions.fulfilled, (state, action) => ({
+        ...state,
+        loading: false,
+        missions: action.payload,
+      }))
+      .addCase(fetchMissions.rejected, (state, action) => ({
+        ...state,
+        loading: false,
+        error: action.error.message,
+      }));
+  },
+});
+
+export default missionsSlice.reducer;
