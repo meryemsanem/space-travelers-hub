3727dbe3a11cb5db277d39c9d6d99d213ba32607
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-08-01 11:37:20 +0300

    Create rocketsSlice for fetching rockets data

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,2 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+const url = 'https://api.spacexdata.com/v3/rockets';
+
+const fetchRocketsData = createAsyncThunk(
+  'rockets/fetchRocketsData',
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('error');
+    }
+  }
+);
+const initialState = {
+  data: [],
+  error: null,
+  isLoading: false,
+};
